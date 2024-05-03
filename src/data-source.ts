cc201034d852415819d655ead31b8eb4928ec674
Mark Levsha <marklevsha4@gmail.com>
2024-05-03 14:57:13 +0300

    change datasource

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const postgresDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
+    host: 'roundhouse.proxy.rlwy.net',
+    port: 34855,
     username: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
+    password: 'CHmpoqgKaHechIRTjFQoDoVkYpaSLhot',
+    database: 'railway',
     entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
     synchronize: false,
     migrations: [__dirname + '/migrations/*{.ts,.js}'],
