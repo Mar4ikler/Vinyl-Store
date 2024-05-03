c0768c5f6fdea02951bf4e674bba9ff91006adc7
Mark Levsha <marklevsha4@gmail.com>
2024-05-03 18:41:07 +0300

    fixes

--- src/strategies/google.strategy.ts
+++ src/strategies/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         super({
             clientID: configService.get('GOOGLE_CLIENT_ID'),
             clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
-            callbackURL: 'http://localhost:4000/user/redirect',
+            callbackURL: configService.get('GOOGLE_REDIRECT_URL'),
             scope: ['email', 'profile'],
         });
     }
